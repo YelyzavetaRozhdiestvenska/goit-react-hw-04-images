4dc79267ebcae9968e739951c9a6f59bbb63bc3f
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2023-11-03 03:23:38 +0100

    http-request and base app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React, { Component } from 'react';
+
+import { fetchImages } from 'api';
+
+
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+  };
+
+async componentDidMount() {
+  const images = await fetchImages();
+  this.setState({images});
+}
+
+
+
+  changeQuery = newQuery => {
+    this.setState({
+      query: `${Date.now()}/${newQuery}`, 
+      images: [],
+      page: 1,
+    });
+  };
+
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.query !== this.state.query || prevState.page !== this.state.page) {
+      alert(
+        `HTTP запрос за ${this.state.query} page=${this.state.page}`
+        );
+      // Не забыть отрезать id/ от query
+      // this.setState({ images: рeзультат запроса})
+    }
+  }
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+
+  render() {
+    return (
+      <div>
+        <header className='searchbar'>
+         <form className='form' onSubmit={(e) => {
+            e.preventDefault();
+            this.changeQuery(e.target.element.query.value);
+            e.target.reset();
+            }}>
+
+            <button type="submit" className='button'>
+              <span className='button-label'>Search</span>
+            </button>
+            <input
+            name='query'
+            className='input'
+            type='text'
+            autoComplete='off'
+            autoFocus
+            placeholder='Search images and photos'/>
+          </form>
+        </header>
+
+      <ul>Gallery
+        <li className='gallery'>
+          <img src='' alt=''/>
+        </li>
+      </ul>
+
+      <div>
+        <button onClick={this.handleLoadMore}>Load more</button>
+      </div>
     </div>
-  );
-};
+    );
+  }
+}
+
