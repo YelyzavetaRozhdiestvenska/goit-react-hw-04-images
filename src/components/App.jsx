3d6d978c5f1814301a8e877f9483fd0f39764408
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2023-11-07 18:49:03 +0100

    HTTP function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import { fetchImages } from 'api';
+import React, { Component } from 'react';
 
 export class App extends Component {
   state = {
@@ -12,98 +12,70 @@ export class App extends Component {
   changeQuery = newQuery => {
     this.setState({
       query: `${Date.now()}/${newQuery}`,
+      // очищаем массив для последующего писка
       images: [],
       page: 1,
     });
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const nextQuery = this.state.query;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
+// Метод жизненного цикла: вызывается при обновлении компонента
+ async componentDidUpdate(prevProps, prevState) {
 
-    if (prevQuery !== nextQuery || prevPage !== nextPage) {
-      console.log(`HTTP запрос за ${nextQuery} page=${nextPage}`);
-      // this.handleSearch();
-
-      // Не забыть отрезать id/ от query
-      // this.setState({ images: рeзультат запроса})
-    };
-  };
-
-  handleSearch = async (e) => {
-    e.preventDefault()
-
-    let searchQuery
-
-    if (e) {
-      searchQuery = e.target.search.value 
-      this.changeQuery(searchQuery)
-    } else {
-      searchQuery = this.state.query.split('/')[1]
-    }
-
-    // const nextQuery = this.state.query;
-    const nextPage = this.state.page;
-
-    try {
-      this.setState({ loading: true });
-      const getImage = await fetchImages(searchQuery, nextPage);
-      if (getImage.length) {
-        this.setState(prevState => ({
-          images:
-            this.state.page > 1 ? [...prevState.images, ...getImage] : getImage,
-        }));
-      } else {
-        this.setState({ loading: false });
-      }
-    } catch (error) {
-      console.log(error);
-      this.setState({ loading: false });
-    }
-  };
-
-  handleSubmit = evt => {
-    alert(evt.target.search.value)
-    evt.preventDefault();
-    this.changeQuery(evt.target.search.value);
-    evt.target.reset();
-  };
-
+if (prevState.query !== this.state.query || prevState.page !== this.state.page ) {
+  console.log(`HTTP запрос за ${this.state.query} page=${this.state.page}`);
   
 
+  // Получаем и добавляем изображения в состояние
+  this.setState({loading: true});
+  const { query, page } = this.state;
+  const actualQuery = query.split('/')[1];
+const getImage = await fetchImages({query: actualQuery, page});    // HTTP запрос за query
+console.log(getImage);
+
+this.setState(prevState => ({                                        // this.setState({images: рузультат запроса})
+  images: this.state.page > 1 ? [prevState.images, ...getImage] : getImage,
+  loading: false,
+})) };
+};
+
+// Загружает следующую порцию картинок
   handleLoadMore = () => {
     this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
+    // const {images, tags, webformatURL, id} = this.state
+    const {loading} = this.state;
     return (
       <div>
         <header className="searchbar">
-          <form className="form" onSubmit={this.handleSearch}>
-            <button className="button">
+          <form className="form" onSubmit={evt => {
+              evt.preventDefault();
+              this.changeQuery(evt.target.elements.query.value);
+              evt.target.reset();
+            }}>
+            <button className="button" >
               <span className="button-label">Search</span>
             </button>
-            <input
+            <input 
               className="input"
               type="text"
               autoComplete="off"
-              name="search"
+              name="query"
               autoFocus
               placeholder="Search images and photos"
             />
           </form>
         </header>
-
-        <ul>
+        {loading ? (<div>Loading</div>) : (<ul> Gallery</ul>)}
+        {/* <ul images={images} key={id}>
           Gallery
-          {/* {images.map(image => (
-            <li className="gallery">
-              <img src={webformatURL} alt={}/>
+          {images.map(image => (
+            <li key={id} image={image} className="gallery">
+              <img src={webformatURL} alt={tags}/>
             </li>
-          ))}  */}
-        </ul>
+          ))}; 
+        </ul> */}
 
         <div>
           <button onClick={this.handleLoadMore}>Load more</button>
@@ -115,54 +87,3 @@ export class App extends Component {
 
 
 
-
-//
-// const API_KEY = '38086992-da778ca69db0828eccdceea4f';
-// axios.defaults.baseURL = `https://pixabay.com/api/`;
-
-// class Test extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       query: '',
-//       images: [],
-//       page: 1,
-//     };
-//   }
-
-//   shouldComponentUpdate(nextProps, nextState) {
-//     if (this.state.query !== nextState.query) return true;
-//     return false;
-//   }
-
-//   handleSearhInput(query) {
-//     this.setState({ query });
-//   }
-
-//   handleSearch() {
-//     const getImage = async () => {
-//       try {
-//         const { data } = await axios.get(
-//           `?q=${this.state.query}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//         );
-//         const { hits } = data;
-//         alert(hits);
-//       } catch (err) {
-//         alert(err);
-//       }
-//     };
-//     getImage();
-//   }
-
-//   render() {
-//     return (
-//       <form>
-//         <input
-//           onChange={e => this.handleSearhInput(e.target.value)}
-//           placeholder="Search..."
-//         />
-//         <button onClick={this.handleSearch.bind(this)}>Searh</button>
-//       </form>
-//     );
-//   }
-// }
