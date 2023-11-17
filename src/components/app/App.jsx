87dd8f398a82bec958c07bf21046aee03957799a
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2023-11-17 19:57:46 +0100

    correction button

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -69,14 +69,14 @@ export class App extends Component {
   };
 
   render() {
-    const { loading, images } = this.state;
+    const { loading, images, page } = this.state;
     return (
       <Wrapper>
         <ToastContainer />
         <Searchbar onSubmit={this.changeQuery} />
         {loading && <Loader />}
         {images.length > 0 && <ImageGallery images={images} />}
-        {images.length > 0 && !loading && (
+        {images.length > 0 && !loading && !page && (
           <Button onClick={this.handleLoadMore}>Load more</Button>
         )}
       </Wrapper>
